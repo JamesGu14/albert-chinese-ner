905756ae283de1af06bb419f2c39665678d8b678
James Gu <gujiasheng@datagrand.com>
2020-05-09 11:16:50 +0800

    update logs

--- albert_ner.py
+++ albert_ner.py
@@ -23,11 +23,21 @@ import tokenization
 import tensorflow as tf
 import pickle
 import tf_metrics
+import logging
 
 # from loss import bi_tempered_logistic_loss
 
-flags = tf.flags
+logger = logging.getLogger('tensorflow')
+logger.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# create file handler which logs even debug messages
+fh = logging.FileHandler('tensorflow.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
+flags = tf.flags
 FLAGS = flags.FLAGS
 
 ## Required parameters
@@ -311,15 +321,15 @@ def convert_single_example(ex_index, example, label_map, max_seq_length, tokeniz
     # assert len(label_mask) == max_seq_length
 
     if ex_index < 5:
-        tf.logging.info("*** Example ***")
-        tf.logging.info("guid: %s" % (example.guid))
-        tf.logging.info("tokens: %s" % " ".join(
+        logger.info("*** Example ***")
+        logger.info("guid: %s" % (example.guid))
+        logger.info("tokens: %s" % " ".join(
             [tokenization.printable_text(x) for x in tokens]))
-        tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-        tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-        tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
-        tf.logging.info("label_ids: %s" % " ".join([str(x) for x in label_ids]))
-        # tf.logging.info("label_mask: %s" % " ".join([str(x) for x in label_mask]))
+        logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+        logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+        logger.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+        logger.info("label_ids: %s" % " ".join([str(x) for x in label_ids]))
+        # logger.info("label_mask: %s" % " ".join([str(x) for x in label_mask]))
 
     feature = InputFeatures(
         input_ids=input_ids,
@@ -345,7 +355,7 @@ def file_based_convert_examples_to_features(
 
     for (ex_index, example) in enumerate(examples):
         if ex_index % 10000 == 0:
-            tf.logging.info(f"Writing example {str(ex_index)} of {str(len(examples))}")
+            logger.info(f"Writing example {str(ex_index)} of {str(len(examples))}")
 
         feature = convert_single_example(ex_index, example, label_map,
                                          max_seq_length, tokenizer, mode)
@@ -469,9 +479,9 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
     def model_fn(features, labels, mode, params):  # pylint: disable=unused-argument
         """The `model_fn` for TPUEstimator."""
 
-        tf.logging.info("*** Features ***")
+        logger.info("*** Features ***")
         for name in sorted(features.keys()):
-            tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
+            logger.info("  name = %s, shape = %s" % (name, features[name].shape))
 
         input_ids = features["input_ids"]
         input_mask = features["input_mask"]
@@ -505,12 +515,12 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
             else:
                 tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
 
-        tf.logging.info("**** Trainable Variables ****")
+        logger.info("**** Trainable Variables ****")
         for var in tvars:
             init_string = ""
             if var.name in initialized_variable_names:
                 init_string = ", *INIT_FROM_CKPT*"
-            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+            logger.info("  name = %s, shape = %s%s", var.name, var.shape,
                             init_string)
 
         output_spec = None
@@ -619,7 +629,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
     features = []
     for (ex_index, example) in enumerate(examples):
         if ex_index % 10000 == 0:
-            tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
+            logger.info("Writing example %d of %d" % (ex_index, len(examples)))
 
         feature = convert_single_example(ex_index, example, label_list,
                                          max_seq_length, tokenizer)
@@ -629,7 +639,7 @@ def convert_examples_to_features(examples, label_list, max_seq_length,
 
 
 def main(_):
-    tf.compat.v1.logging.set_verbosity(tf.logging.INFO)
+    # tf.compat.v1.logging.set_verbosity(logger.INFO)
 
     processors = {
         # TODO change processors
@@ -719,10 +729,10 @@ def main(_):
         if not train_file_exists:  # if tf_record file not exist, convert from raw text file. # TODO
             file_based_convert_examples_to_features(train_examples, label_list, FLAGS.max_seq_length, tokenizer,
                                                     train_file)
-        tf.logging.info("***** Running training *****")
-        tf.logging.info("  Num examples = %d", len(train_examples))
-        tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
-        tf.logging.info("  Num steps = %d", num_train_steps)
+        logger.info("***** Running training *****")
+        logger.info("  Num examples = %d", len(train_examples))
+        logger.info("  Batch size = %d", FLAGS.train_batch_size)
+        logger.info("  Num steps = %d", num_train_steps)
         train_input_fn = file_based_input_fn_builder(
             input_file=train_file,
             seq_length=FLAGS.max_seq_length,
@@ -746,11 +756,11 @@ def main(_):
         file_based_convert_examples_to_features(
             eval_examples, label_list, FLAGS.max_seq_length, tokenizer, eval_file)
 
-        tf.logging.info("***** Running evaluation *****")
-        tf.logging.info("  Num examples = %d (%d actual, %d padding)",
+        logger.info("***** Running evaluation *****")
+        logger.info("  Num examples = %d (%d actual, %d padding)",
                         len(eval_examples), num_actual_eval_examples,
                         len(eval_examples) - num_actual_eval_examples)
-        tf.logging.info("  Batch size = %d", FLAGS.eval_batch_size)
+        logger.info("  Batch size = %d", FLAGS.eval_batch_size)
 
         # This tells the estimator to run through the entire set.
         eval_steps = None
@@ -776,21 +786,21 @@ def main(_):
                 ckpt_name = filename[:-6]
                 cur_filename = os.path.join(FLAGS.output_dir, ckpt_name)
                 global_step = int(cur_filename.split("-")[-1])
-                tf.logging.info("Add {} to eval list.".format(cur_filename))
+                logger.info("Add {} to eval list.".format(cur_filename))
                 steps_and_files.append([global_step, cur_filename])
         steps_and_files = sorted(steps_and_files, key=lambda x: x[0])
 
         output_eval_file = os.path.join(FLAGS.data_dir, "eval_results_albert_zh.txt")
         print("output_eval_file:", output_eval_file)
-        tf.logging.info("output_eval_file:" + output_eval_file)
+        logger.info("output_eval_file:" + output_eval_file)
         with tf.gfile.GFile(output_eval_file, "w") as writer:
             for global_step, filename in sorted(steps_and_files, key=lambda x: x[0]):
                 result = estimator.evaluate(input_fn=eval_input_fn, steps=eval_steps, checkpoint_path=filename)
 
-                tf.logging.info("***** Eval results %s *****" % (filename))
+                logger.info("***** Eval results %s *****" % (filename))
                 writer.write("***** Eval results %s *****\n" % (filename))
                 for key in sorted(result.keys()):
-                    tf.logging.info("  %s = %s", key, str(result[key]))
+                    logger.info("  %s = %s", key, str(result[key]))
                     writer.write("%s = %s\n" % (key, str(result[key])))
         #######################################################################################################################
 
@@ -798,9 +808,9 @@ def main(_):
         #
         # output_eval_file = os.path.join(FLAGS.output_dir, "eval_results.txt")
         # with tf.gfile.GFile(output_eval_file, "w") as writer:
-        #  tf.logging.info("***** Eval results *****")
+        #  logger.info("***** Eval results *****")
         #  for key in sorted(result.keys()):
-        #    tf.logging.info("  %s = %s", key, str(result[key]))
+        #    logger.info("  %s = %s", key, str(result[key]))
         #    writer.write("%s = %s\n" % (key, str(result[key])))
 
     if FLAGS.do_predict:
@@ -817,9 +827,9 @@ def main(_):
                                                 FLAGS.max_seq_length, tokenizer,
                                                 predict_file, mode="test")
 
-        tf.logging.info("***** Running prediction*****")
-        tf.logging.info("  Num examples = %d", len(predict_examples))
-        tf.logging.info("  Batch size = %d", FLAGS.predict_batch_size)
+        logger.info("***** Running prediction*****")
+        logger.info("  Num examples = %d", len(predict_examples))
+        logger.info("  Batch size = %d", FLAGS.predict_batch_size)
         if FLAGS.use_tpu:
             # Warning: According to tpu_estimator.py Prediction on TPU is an
             # experimental feature and hence not supported here
